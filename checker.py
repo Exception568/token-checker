df4555bb91202130ca7e4fbf9209f89945b42d79
Exception568 <hashcatx568@gmail.com>
2022-11-05 19:05:26 +0530

    Update checker.py

--- checker.py
+++ checker.py
@@ -15,11 +15,9 @@ Write.Input("[Press enter to start checking the tokens]:",Colors.blue_to_cyan)
 with open("tokens.txt", "r") as file:
         tokens = [line.replace('\n', '') for line in file.readlines() if line != '\n']
         for token in tokens:
-            k = token.split(".")
-            p = len(k[1:2])
             m = "*"
             r1 = requests.get('https://discord.com/api/v6/auth/login', headers={"Authorization": token})
             if r1.status_code <= 299:
-                Write.Print(f"Valid token {k[0]}{m*26}\n",Colors.blue_to_purple)
+                Write.Print(f"Valid token {token}\n",Colors.blue_to_purple)
             else:
-                Write.Print(f"Invalid token {k[0]}{m*26}\n",Colors.blue_to_red)
+                Write.Print(f"Invalid token {token}\n",Colors.blue_to_red)
